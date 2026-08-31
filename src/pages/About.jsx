f3639504050c0c78a61ba66cbc9b825d661890b4
Rekha <rekhavs003@gmail.com>
2026-08-30 23:24:03 -0500

    updated home page css styling

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,33 +1,39 @@
 import React from "react";
+import "./About.css";
+
 function About() {
   return (
-    <section className="page">
-      <div className="card">
+    <section className="about-page">
+      <div className="about-card">
+
         <h1>About Mood Tracker</h1>
 
-        <p>
-          Mood Tracker is a beginner-friendly mood and weather journal.
-          It allows users to record how they feel each day and add
-          information about the weather and their thoughts.
+        <p className="about-introduction">
+          Mood Tracker is a beginner-friendly mood journal.It allows you to record how you feel each day and add information
+          about your thoughts.
         </p>
 
-        <h2>What can you do?</h2>
+        <div className="about-section">
+          <h2>What can you do?</h2>
 
-        <ul>
-          <li>Add a daily mood entry.</li>
-          <li>Choose the weather.</li>
-          <li>Write a short note.</li>
-          <li>Edit an entry.</li>
-          <li>Delete an entry.</li>
-          <li>View entries on a calendar.</li>
-        </ul>
+          <ul>
+            <li>Add a daily mood entry.</li>
+            <li>Write a short note.</li>
+            <li>Edit an entry.</li>
+            <li>Delete an entry.</li>
+            <li>View entries on a calendar.</li>
+          </ul>
+        </div>
 
-        <h2>Why use it?</h2>
+        <div className="about-section">
+          <h2>Why use it?</h2>
 
-        <p>
-          A simple journal can help you pause, reflect, and become
-          more aware of your daily experiences.
-        </p>
+          <p>
+            A simple journal can help you pause, reflect, and become more
+            aware of your daily experiences.
+          </p>
+        </div>
+
       </div>
     </section>
   );
